549b866136d28e4fb6c9c20f4f1865a402975f1f
Emanuele Simula <simulaemanuele@gmail.com>
2022-09-22 12:55:53 +0200

    exercise: Apply the singleton pattern done.

---
+++ design-pattern/singleton/counter.mjs
@@ -0,0 +1,7 @@
+export const counterInstance = {
+    count: 0,
+    increment() {
+        this.count++;
+    },
+};
+
---
+++ design-pattern/singleton/index.mjs
@@ -0,0 +1,8 @@
+import { counterInstance } from "./counter.mjs";
+import "./script-1.mjs";
+import "./script-2.mjs";
+
+counterInstance.increment();
+
+console.log("Count", counterInstance.count);
+
